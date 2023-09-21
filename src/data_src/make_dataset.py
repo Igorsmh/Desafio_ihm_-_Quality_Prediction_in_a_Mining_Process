368894ca5c6845b7f1e500b50fb637d69361b27e
Igor <shenriques@ihm.com.br>
2023-09-21 13:01:34 -0300

    refactor

--- src/data_src/make_dataset.py
+++ src/data_src/make_dataset.py
@@ -1,6 +1,9 @@
 import pandas as pd
+""" Make imports 
 
-def make_dataset() -> pd.DataFrame:
+ """
+
+def generate_dataset_base() -> pd.DataFrame:
     """
     Reads a CSV file and creates a dataset for the mining process flotation plant.
 
@@ -11,5 +14,4 @@ def make_dataset() -> pd.DataFrame:
     df = pd.read_csv(r"data\raw\MiningProcess_Flotation_Plant_Database.csv",decimal=",")
     dataset = df.copy()
     dataset["date"] = pd.to_datetime(dataset["date"])
-    
     return dataset.to_csv("data\interim\MiningProcess_Flotation_Plant_Database.csv", index=False)
