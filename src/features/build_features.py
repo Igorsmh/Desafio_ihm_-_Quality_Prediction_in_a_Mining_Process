51d700300789209a827ced53d9fc61ff39df0b00
Igor <shenriques@ihm.com.br>
2023-09-22 01:27:23 -0300

    feature(src)-add de funcs para lag e limpeza

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,15 +1,45 @@
+""" imports """
 import pandas as pd
 
 
-def generate_week() -> pd.DataFrame:
+def generate_week(dataframe) -> pd.DataFrame:
     """
-    Generate a week dataset from the MiningProcess_Flotation_Plant_Database.
+   Generate a week column in the dataset based on the date column.
+   
+   Parameters:
+       dataset (pd.DataFrame): The input dataset.
+   
+   Returns:
+       pd.DataFrame: The dataset with an additional "Dia_da_semana" column.
+   """
+    dataframe["Dia_da_semana"] = dataframe["date"].dt.day_name()
+    return  dataframe
 
-    Returns:
-        pd.DataFrame: The dataset with the day name.
+
+def generate_dataset_train(dataframe) -> pd.DataFrame:
     """
+	Generate a training dataset from the given dataframe.
 
-    dataset_semana = pd.read_csv(r"data\interim\MiningProcess_Flotation_Plant_Database.csv",decimal=",")
+	Parameters:
+	- dataframe (pd.DataFrame): The input dataframe.
 
-    dataset_semana["Dia_da_semana"] = dataset_semana["date"].dt.day_name()
-    return  dataset_semana.to_csv("data\processed\dataset_semana.csv", index=False)
+	Returns:
+	- pd.DataFrame: The generated training dataset.
+	"""
+
+    dataframe_modified = dataframe.copy()
+    dataframe_modified['% Silica Concentrate'] = dataframe_modified['% Silica Concentrate'].round(2)
+    dataframe_modified = dataframe_modified.drop_duplicates(["% Iron Feed","% Silica Feed"])
+    return  dataframe_modified
+
+
+def generate_lag (dataframe)-> pd.DataFrame:
+    """
+	Create a lagged version of a column "% Silica Concentrate".
+	
+	:param dataframe: The input DataFrame.
+	:return: The lagged DataFrame with one row shifted up.
+	"""
+
+    dataframe["%Silica_Concentrate_anterior"] = dataframe["% Silica Concentrate"].shift(1)
+    return dataframe
