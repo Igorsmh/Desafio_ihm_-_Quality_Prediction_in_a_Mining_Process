d4034bbecd3018478ef4843a2585317263677abd
Igor <shenriques@ihm.com.br>
2023-09-22 01:26:22 -0300

    improvement(src)-mudança de nomeclatura

--- src/data_src/make_dataset.py
+++ src/data_src/make_dataset.py
@@ -1,17 +1,17 @@
+""" Make imports  """
 import pandas as pd
-""" Make imports 
-
- """
 
 def generate_dataset_base() -> pd.DataFrame:
     """
-    Reads a CSV file and creates a dataset for the mining process flotation plant.
+    Reads a CSV file and modifies a dataset for the mining process flotation plant, chanding the date format.
 
     Returns:
         pandas.DataFrame: The created dataset.
     """
 
-    df = pd.read_csv(r"data\raw\MiningProcess_Flotation_Plant_Database.csv",decimal=",")
-    dataset = df.copy()
+    data = pd.read_csv(r"data\raw\MiningProcess_Flotation_Plant_Database.csv",decimal=",")
+    dataset = data.copy()
     dataset["date"] = pd.to_datetime(dataset["date"])
     return dataset.to_csv("data\interim\MiningProcess_Flotation_Plant_Database.csv", index=False)
+
+
