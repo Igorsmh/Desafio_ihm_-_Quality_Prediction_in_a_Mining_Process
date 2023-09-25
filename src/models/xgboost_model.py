7bbd09a3a74970b09be0d8b9c6ad387bca7b0b22
Igor <shenriques@ihm.com.br>
2023-09-25 13:43:59 -0300

    refactor(src)-ajuste nos arquivos

--- src/models/xgboost_model.py
+++ src/models/xgboost_model.py
@@ -1,46 +1,50 @@
 #imports
+# pylint: disable=E0401
 import pandas as pd
 import xgboost as xgb
+import numpy as np
 from sklearn.metrics import mean_squared_error
-from src.features.build_features import generate_week ,\
-      generate_dataset_train, generate_lag
+from src.features.build_features_xgboost\
+     import generate_time , generate_dataset_train, generate_lag
+
 
 
 # Carregando o dataset
-df =\
-pd.read_csv("data\interim\MiningProcess_Flotation_Plant_Database.csv")
+df = pd.read_csv(r"data\interim\MiningProcess_Flotation_Plant_Database.csv")
+df = df.set_index('date')
 
-""" Dividindo a base, de forma que tenhamos,
-      aprox 80% para treino e 20% para teste.
- Para não ocorrer overfiting entre a semana de mudança,
-    aumentei aproximadamente 20 linhas da amostra.
 """
+Dividindo a base, de forma que tenhamos,aprox 80% para treino e 20% para teste.
+
+Para não ocorrer overfiting entre a semana de mudança, aumentei aproximadamente 20 linhas da amostra.
+"""
+
 # Divisão para o treino 
 train = df.loc[df.index < '2017-07-24 01:00:00' ]
-
 # Divisão para o teste 
 test = df.loc[df.index >= '2017-08-20 01:00:00' ]
 
-# Criando novas features no treino 
-train = generate_week(train)
-train = generate_lag(train)
-
-# Criando novas features no teste
-test = generate_week(test)
-test = generate_lag(test)
 
 # Realizando um tratamento na base de treino 
 train = generate_dataset_train(train)
+# Criando novas features no treino 
+train = generate_time(train)
+train = generate_lag(train)
+# Criando novas features no teste
+test = generate_time(test)
+test = generate_lag(test)
+
 
 
 
 # Definindo features que serão utilizadas no modelo e qual a target.
 
-X_train = train.drop("% Iron Concentrate","% Silica Concentrate",)
+X_train = train.drop(["% Iron Concentrate","% Silica Concentrate"],axis=1)
 y_train = train["% Silica Concentrate"]
 
-X_test = test.drop("% Iron Concentrate","% Silica Concentrate",)
+X_test = test.drop(["% Iron Concentrate","% Silica Concentrate"],axis=1)
 y_test = test["% Silica Concentrate"]
+
 
 # Usando xGBoost
 reg = xgb.XGBRegressor(base_score=0.5, booster='gbtree',    
@@ -50,15 +54,14 @@ reg = xgb.XGBRegressor(base_score=0.5, booster='gbtree',
                        max_depth=3,
                        learning_rate=0.01)
 
+
 # Treinando o modelo
 reg.fit(X_train, y_train,
         eval_set=[(X_train, y_train), (X_test, y_test)],
         verbose=100)
 
 
-# Previsões
-ax = df[['consumo_mw']].plot(figsize=(15, 5))
-df['consumo_previsto'].plot(ax=ax, style='.')
-plt.legend(['Dados Reais', 'Previsões'])
-ax.set_title('Dados reais vs Previsões')
-plt.show()
+test['% silica_prevista'] = reg.predict(X_test)
+
+score = np.sqrt(mean_squared_error(test['% Silica Concentrate'], test['% silica_prevista']))
+print(f'RMSE Score no teste: {score:0.2f}')
